85d41e89617a57ce2876806173c852934e8a1c8b
HTY <1581332872@163.com>
2019-08-27 17:53:08 +0800

    vdom study

---
+++ vdom/vdom_ex1.js
@@ -0,0 +1,21 @@
+let vdom = {
+    tag:'ul',
+    attrs:{
+        id:'list'
+    },
+    children:[
+        {
+            tag:'li',
+            attrs:{
+                className:'item'
+            },
+            children:['Item 1']
+        },{
+        tag:'li',
+            attrs:{
+            className:'item'
+            },
+            children:['Item2']
+        }
+    ]
+}
