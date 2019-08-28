6a3885fb0f14e385739c0898e08f6756b26a8010
HTY <1581332872@163.com>
2019-08-28 11:52:50 +0800

    vdom_jquery

--- vdom/vdom_ex1/vdom_ex1.js
+++ vdom/vdom_ex1/vdom_ex1.js
@@ -16,6 +16,23 @@ let vdom = {
             className:'item'
             },
             children:['Item2']
-        }
-    ]
+        }]
 }
+
+let vdom2 = [
+    {
+        name:'张三',
+        age:'20',
+        address:'北京'
+    },
+    {
+        name:'李四',
+        age:'21',
+        address:'上海'
+    },
+    {
+        name:'王五',
+        age:'22',
+        address:'广州'
+    }
+]
