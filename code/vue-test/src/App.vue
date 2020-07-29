ba6e32e45f0916d92263e072144b2659c6d9e63d
sakurahuang <sakurahuang@tencent.com>
2020-07-29 11:22:07 +0800

    组件注册

--- code/vue-test/src/App.vue
+++ code/vue-test/src/App.vue
@@ -19,7 +19,7 @@ export default {
   },
   data:function() {
     return {
-      message:'bacra'
+      message:'dilraba'
     };
   }
 }
--- code/vue-test/src/main.js
+++ code/vue-test/src/main.js
@@ -1,33 +1,38 @@
 import Vue from 'vue/dist/vue.esm.js'
-// import App from './App.vue'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
-let childComp = {
-  template: '<div>{{msg}}</div>',
-  created () {
-    console.log('child created')
-  },
-  mounted () {
-    console.log('child mounted')
-  },
-  data () {
-    return {
-      msg: 'Hello dilraba'
-    }
-  }
-}
-
-Vue.mixin({
-  created () {
-    console.log('parent created')
-  }
-})
-
+Vue.component('app', App)
 new Vue ({
-  el:'#app', 
-  render: h => h(childComp)
+  el: '#app',
+  template: '<app></app>'
 })
+// let childComp = {
+//   template: '<div>{{msg}}</div>',
+//   created () {
+//     console.log('child created')
+//   },
+//   mounted () {
+//     console.log('child mounted')
+//   },
+//   data () {
+//     return {
+//       msg: 'Hello dilraba'
+//     }
+//   }
+// }
+
+// Vue.mixin({
+//   created () {
+//     console.log('parent created')
+//   }
+// })
+
+// new Vue ({
+//   el:'#app', 
+//   render: h => h(childComp)
+// })
 
 // 引入APP组件
 // new Vue({
